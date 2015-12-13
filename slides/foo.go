90a40d6957d5ab044a0b04abd5d5f36756e35cad
duongnt <ntduong268@gmail.com>
2015-12-13 12:55:59 +0900

    update sample code in foo.go

--- slides/foo.go
+++ slides/foo.go
@@ -1,8 +1,36 @@
 // Sample code @vnitc
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unsafe"
+)
+
+func foo() *int {
+	t := 2
+	return &t
+}
+
+type MyInt64 int64
+
+func (mi MyInt64) dump() {
+	fmt.Println(mi)
+}
 
 func main() {
+	// Go can handle kanji and more :)
 	fmt.Println("合コンにいこう！")
+
+	x := foo()
+	fmt.Println(*x)
+
+	// t := 2
+	// fmt.Println(t++)
+	// fmt.Println(z := 3)
+
+	// var t MyInt64 = 10
+	// fmt.Println(t)
+	// var t2 int64 = t
+
+	fmt.Println(unsafe.Sizeof(struct{}{}))
 }
